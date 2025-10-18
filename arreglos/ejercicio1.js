6a1c74fb845acfec1e713fd056ab01662ebefa40
Alejandr02608 <maciasalcivarjosealejandro70@gmail.com>
2025-10-17 20:01:02 -0500

    mejoracion de la tabulacion en el codigo

--- arreglos/ejercicio1.js
+++ arreglos/ejercicio1.js
@@ -1,42 +1,67 @@
-let notas=[];
+let notas = [];
 
-agregarElementos = function(){
+/**
+ * Agrega valores de ejemplo al arreglo.
+ */
+agregarElementos = function () {
     notas.push(5);
     notas.push(10);
-    console.log(notas.length);
-}
-recorrerArreglo=function(){
-    let notaR
-    for(let indice=0; indice<notas.length;indice++){
-        notaR=notas[indice]
-        console.log(notaR);
+    console.log("Notas agregadas automáticamente:", notas.length);
+};
+
+/**
+ * Recorre el arreglo y muestra las notas en consola.
+ */
+recorrerArreglo = function () {
+    for (let indice = 0; indice < notas.length; indice++) {
+        console.log("Nota:", notas[indice]);
     }
-}
+};
 
-probarAgregarNotas = function(){
-    let notarecuperada;
-    notarecuperada = recuperarInt("txtNota");
-    agregarNotas(notarecuperada);
-}
+/**
+ * Recupera la nota desde la caja de texto y la agrega al arreglo.
+ */
+probarAgregarNotas = function () {
+    let notaRecuperada = recuperarInt("txtNota");
+    agregarNotas(notaRecuperada);
+    console.log("Nota agregada:", notaRecuperada);
+};
 
-
-agregarNotas = function(nota){
+/**
+ * Agrega una nota al arreglo.
+ */
+agregarNotas = function (nota) {
     notas.push(nota);
+};
 
-}
+/**
+ * Calcula y retorna el promedio del arreglo de notas.
+ */
+calcularPromedio = function () {
+    let sumaNotas = 0;
+    let promedio = 0;
 
-calcularPromedio=function(){
-    let sumaNotas =0;
-    let promedio=0;
-    for(let indice=0; indice<notas.length;indice++){
-        sumaNotas=sumaNotas+notas[indice];
+    if (notas.length === 0) {
+        return 0; // Evita dividir por cero
     }
-    promedio=sumaNotas/notas.length;
+
+    for (let indice = 0; indice < notas.length; indice++) {
+        sumaNotas = sumaNotas + notas[indice];
+    }
+
+    promedio = sumaNotas / notas.length;
     return promedio;
-    }
+};
 
-    ejecutarPromedio = function(){
-    let promedioCalculado = calcularPromedio(); // llamamos la función
-    mostrarTexto("lblPromedio", "Promedio: " + promedioCalculado.toFixed(2)); // mostramos
-}
-    
+/**
+ * Ejecuta el cálculo del promedio y lo muestra en pantalla.
+ */
+ejecutarPromedio = function () {
+    let promedioCalculado = calcularPromedio();
+
+    if (promedioCalculado === 0 && notas.length === 0) {
+        mostrarTexto("lblPromedio", "Promedio: No hay notas ingresadas");
+    } else {
+        mostrarTexto("lblPromedio", "Promedio: " + promedioCalculado.toFixed(2));
+    }
+};
