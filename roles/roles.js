81055956d1e84a57d7d374dfd3851d5b347b4345
Alejandr02608 <maciasalcivarjosealejandro70@gmail.com>
2025-10-21 21:13:18 -0500

    roles parte 3 funciones implementadas

--- roles/roles.js
+++ roles/roles.js
@@ -44,3 +44,62 @@ mostrarEmpleados = function() {
     cmpTabla.innerHTML = contenidoTabla;
 }
 
+function ejecutarNuevo() {
+    habilitarComponente("txtCedula");
+    habilitarComponente("txtNombre");
+    habilitarComponente("txtApellido");
+    habilitarComponente("txtSueldo");
+    habilitarComponente("btnGuardar");
+    esNuevo = true;
+}
+
+
+function buscarEmpleado(cedula) {
+    for (let i = 0; i < empleados.length; i++) {
+        if (empleados[i].cedula === cedula) {
+            return empleados[i];
+        }
+    }
+    return null;
+}
+
+
+function agregarEmpleado(empleado) {
+    if (buscarEmpleado(empleado.cedula) === null) {
+        empleados.push(empleado);
+        return true;
+    }
+    return false;
+}
+
+
+function guardar() {
+    let cedula = recuperarTexto("txtCedula");
+    let nombre = recuperarTexto("txtNombre");
+    let apellido = recuperarTexto("txtApellido");
+    let sueldo = recuperarFloat("txtSueldo");
+
+    if (cedula.length < 3 || isNaN(parseInt(cedula)) ||
+        nombre.length < 3 || !isNaN(nombre) ||
+        isNaN(sueldo)) {
+        mostrarTexto("lblErrorCedula", "Error en las validaciones");
+        return;
+    }
+
+    if (esNuevo) {
+        let nuevoEmpleado = {
+            cedula: cedula,
+            nombre: nombre,
+            apellido: apellido,
+            sueldo: sueldo
+        };
+
+        if (agregarEmpleado(nuevoEmpleado)) {
+            mostrarTexto("lblErrorCedula", "EMPLEADO GUARDADO CORRECTAMENTE");
+            mostrarEmpleados();
+            deshabilitarCamposEmpleado();
+        } else {
+            mostrarTexto("lblErrorCedula", "YA EXISTE UN EMPLEADO CON LA CÉDULA " + cedula);
+        }
+    }
+}
